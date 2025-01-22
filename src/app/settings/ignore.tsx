deba3c7c8c35719a934662343e9e57abc7338606
David Maguire <drmaguire15@gmail.com>
2025-01-22 16:40:41 -0500

    Basic Setup
    My first commit, wow. What a journey this will be...

---
+++ src/app/settings/ignore.tsx
@@ -0,0 +1,14 @@
+//test layout, named layout.tsx
+export default function PageLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ src/app/settings/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+    return (
+        <main>
+            <p className="text-2xl font-mono hover:font-serif">This is my TEST page</p>
+        </main>
+);
+}
