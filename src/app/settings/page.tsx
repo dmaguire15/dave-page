2ea655dc0b64a85f1464af550a813054d763e4f0
David Maguire <drmaguire15@gmail.com>
2025-01-22 17:06:52 -0500

    Added additional CSS stuff

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,8 +1,12 @@
 
 export default function Page() {
     return (
-        <main>
-            <p className="text-2xl font-mono hover:font-serif">This is my TEST page</p>
+        //p = padding
+        //m = margin | ml = margin left | mt = margin top
+        <main id="content" className="container mx-auto max-w-screen-xxl bg-gray-300">
+            <p className="text-4xl text-center p-5 text-black font-mono hover:font-serif hover:italic">
+                This is Dave's TEST page
+                </p>
         </main>
 );
 }
